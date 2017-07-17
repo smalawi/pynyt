91a609c59b6956efbfcc07860216f067ff2cf4e9
smalawi <sayedtwo@mac.com>
2017-07-17 15:05:17 +0300

    Paging now functional

--- pynyt/pynyt.py
+++ pynyt/pynyt.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import sys
+import time
+
 
 class NYTArticleAPIObject:
     def __init__(self, api_key):
@@ -16,16 +18,25 @@ class NYTArticleAPIObject:
                 valid_params.index(param)
             except ValueError:
                 print("Invalid query parameter: ", param)
+                sys.exit(1)
 
-    # Get headlines for first 10 articles
-    def query(self, params={}, all_pages=False):
-        r = requests.get(self.url, headers={'api-key': self.api_key}, params=params)
-        parsed_json = json.loads(r.text)
-
+    # Get headlines
+    def query(self, params={}, all_pages=True):
         self.check_params(params)
-
         headlines = []
-        for article in parsed_json['response']['docs']:
-            headlines.append(article['headline']['main'])
+        max_page = 1
+        if all_pages:
+            max_page = 100
+
+        for page_num in range(0, max_page):
+            params['page'] = page_num
+            r = requests.get(self.url, headers={'api-key': self.api_key}, params=params)
+            parsed_json = json.loads(r.text)
+            print(parsed_json)
+            if len(parsed_json['response']['docs']) == 0:
+                return headlines
+            for article in parsed_json['response']['docs']:
+                headlines.append(article['headline']['main'])
+            time.sleep(1)
 
         return headlines
--- tests/test.py
+++ tests/test.py
@@ -4,5 +4,11 @@ import os
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'pynyt'))
 import pynyt
 
-apiobj = pynyt.NYTArticleAPIObject("43207e6b68494e4184584461a82b3aa2")
-print(apiobj.query(params={'paaefege': 5, 'efaefe': 'feeeee'}))
+api_obj = pynyt.NYTArticleAPIObject("xxxx")
+headlines = api_obj.query(params={'q': 'Swarthmore College',
+                                  'begin_date': '20160717'})
+
+for headline in headlines:
+    print(headline)
+
+print(len(headlines))

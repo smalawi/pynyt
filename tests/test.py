92bf13d5e619d9b4cf3f12b00c6ee7860a9dd26f
smalawi <sayedtwo@mac.com>
2017-08-15 10:27:11 +0300

    update test

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,18 @@
 import sys 
 import os
+import json
 
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'pynyt'))
 import pynyt
 
-api_obj = pynyt.NYTArticleAPIObject("xxxx")
-headlines = api_obj.query(params={'q': 'Swarthmore College',
-                                  'begin_date': '20160717'})
+api_obj = pynyt.NYTArticleAPIObject("*****")
 
-for headline in headlines:
-    print(headline)
+# print(api_obj.get_usage())
+results = api_obj.query(begin_date = 20170720,
+                        page = 0,
+                        facet_field = ['day_of_week',
+                                       'sectiiion_name'])
 
-print(len(headlines))
+for page in results:
+    for facet in page['response']['facets']:
+        print(facet)
